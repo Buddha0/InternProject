92198646a55536abf9fa80961c68f14b935d4849
Buddha <buddhalama88480@gmail.com>
2024-03-09 04:43:39 -0800

    added funcionality and fixed styling

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+
+//faq
 function toggleAnswer(question) {
 
     let answer = question.nextElementSibling;
     let allAnswers = document.querySelectorAll('.answer');
 
-    allAnswers.forEach(function(ans) {
-   
+    allAnswers.forEach(function (ans) {
+
         if (ans !== answer) {
             ans.classList.add("answer-hidden");
             ans.previousElementSibling.classList.remove("open-question");
@@ -12,34 +14,74 @@ function toggleAnswer(question) {
         }
     });
 
-answer.classList.toggle("answer-hidden");    
- question.classList.toggle("open-question")
- question.classList.toggle("open-faq")
+    answer.classList.toggle("answer-hidden");
+    question.classList.toggle("open-question")
+    question.classList.toggle("open-faq")
 }
 
 
-// Select the image buttons
-const imageButtons = document.querySelectorAll('.image-button');
 
-// Select all slider cards
+
+//image rotator
+const imageButtons = document.querySelectorAll('.image-button');
 const sliderCards = document.querySelectorAll('.slider-card');
 
-// Iterate through each image button and attach a click event listener
+
 imageButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        // Remove 'active' class from all buttons
+
         imageButtons.forEach(btn => btn.classList.remove('active'));
-        // Add 'active' class to the clicked button
+
         button.classList.add('active');
-        
-        // Remove 'active' class from all slider cards
+
+
         sliderCards.forEach(card => card.classList.remove('active', 'left', 'right'));
-        
-        // Add 'active' class to the corresponding slider card
+
+
         sliderCards[index].classList.add('active');
-        // Add 'previous' class to the previous slider card
         sliderCards[(index - 1 + sliderCards.length) % sliderCards.length].classList.add('left');
-        // Add 'next' class to the next slider card
         sliderCards[(index + 1) % sliderCards.length].classList.add('right');
+    });
+});
+
+
+// tabs | links
+
+const links = document.querySelectorAll(".link");
+const arr = [
+    {
+        titleOne: "Goku",
+        titleTwo: "Vegeta",
+        image:"./images/dbz-medium.jpg"
+      
+    },
+    {
+        titleOne: "Mechamaru",
+        titleTwo: "Robo",
+        image:"./images/mecha-medium.jpg"
+     
+    },
+    {
+        titleOne: "Yoda",
+        titleTwo: "Baby Yoda",
+        image:"./images/yoda-medium.jpg"
+    
+    }
+];
+
+links.forEach((link, index) => {
+    link.addEventListener("click", () => {
+   
+        const selectedContent = arr[index];
+        console.log(selectedContent)
+        document.querySelector('.dynamic-heading').innerText = selectedContent.titleOne;
+        document.querySelector('.dynamic-heading-two').innerText = selectedContent.titleTwo;
+        document.querySelector(".dynamic-image").src = selectedContent.image
+
+        links.forEach((link)=>{
+            link.classList.remove("active-link")
+        })
+
+        link.classList.add("active-link")
     });
 });
